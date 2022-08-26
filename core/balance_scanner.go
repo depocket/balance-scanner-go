19079825281f056ed9208bb3b0d92f12bfc80d02
Himitsuko <himitsuko@github.com>
2022-08-26 13:06:50 +0700

    add: Batchcall to optimize token balance query performances

--- core/balance_scanner.go
+++ core/balance_scanner.go
@@ -4,6 +4,12 @@
 package core
 
 import (
+	"context"
+	"fmt"
+	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/rpc"
 	"math/big"
 	"strings"
 
@@ -16,45 +22,56 @@ var BalanceScannerMetaData = &bind.MetaData{
 	ABI: "[{\"inputs\":[{\"internalType\":\"address\",\"name\":\"account\",\"type\":\"address\"},{\"internalType\":\"contractIBEP20[]\",\"name\":\"tokens\",\"type\":\"address[]\"}],\"name\":\"getBalances\",\"outputs\":[{\"internalType\":\"uint256[]\",\"name\":\"balances\",\"type\":\"uint256[]\"}],\"stateMutability\":\"view\",\"type\":\"function\"}]",
 }
 
+type CallArg struct {
+	From     common.Address   `json:"from"`
+	To       common.Address  `json:"to"`
+	Gas      string         `json:"gas"`
+	GasPrice string         `json:"gasPrice"`
+	Value    string          `json:"value"`
+	Data     string           `json:"data"`
+	Nonce    string          `json:"nonce"`
+}
+
 var BalanceScannerABI = BalanceScannerMetaData.ABI
 
 type BalanceScanner struct {
-	contract *bind.BoundContract
+	Client          *ethclient.Client
+	RpcClient          *rpc.Client
+	ContractAddress common.Address
+	_abi             abi.ABI
 }
 
-func NewBalanceScanner(address string, backend bind.ContractBackend) (*BalanceScanner, error) {
-	contract, err := bindBalanceScanner(common.HexToAddress(address), backend, backend, backend)
-	if err != nil {
-		return nil, err
-	}
-	return &BalanceScanner{contract: contract}, nil
+func NewBalanceScanner(address string, client *ethclient.Client, rpcClient *rpc.Client) (*BalanceScanner, error) {
+	_abi, _ := abi.JSON(strings.NewReader(BalanceScannerABI))
+	return &BalanceScanner{
+		_abi: _abi,
+		Client: client,
+		RpcClient: rpcClient,
+		ContractAddress: common.HexToAddress(address),
+	}, nil
 }
 
-func bindBalanceScanner(address common.Address, caller bind.ContractCaller, transactor bind.ContractTransactor, filterer bind.ContractFilterer) (*bind.BoundContract, error) {
-	parsed, err := abi.JSON(strings.NewReader(BalanceScannerABI))
-	if err != nil {
-		return nil, err
-	}
-	return bind.NewBoundContract(address, parsed, caller, transactor, filterer), nil
-}
-
-func (_BalanceScanner *BalanceScanner) GetBalances(opts *bind.CallOpts, account common.Address, tokens []common.Address) ([]*big.Int, error) {
+func (_BalanceScanner *BalanceScanner) GetBalances(account common.Address, tokens []common.Address) ([]*big.Int, error) {
 	var out []interface{}
-	err := _BalanceScanner.contract.Call(opts, &out, "getBalances", account, tokens)
-
+	callData, err := _BalanceScanner._abi.Pack("getBalances", account, tokens)
 	if err != nil {
 		return *new([]*big.Int), err
 	}
-
+	resp, err := _BalanceScanner.Client.CallContract(context.Background(), ethereum.CallMsg{To: &_BalanceScanner.ContractAddress, Data: callData}, nil)
+	if err != nil {
+		return *new([]*big.Int), err
+	}
+	out, err = _BalanceScanner._abi.Unpack("getBalances", resp)
+	if err != nil {
+		return *new([]*big.Int), err
+	}
 	out0 := *abi.ConvertType(out[0], new([]*big.Int)).(*[]*big.Int)
-
 	return out0, err
 }
 
-
 func (_BalanceScanner *BalanceScanner) TokenMapBalances(address common.Address, tokens []common.Address) (map[string]*big.Int, error) {
 	balances := make(map[string]*big.Int)
-	res, err := _BalanceScanner.GetBalances(&bind.CallOpts{}, address, tokens)
+	res, err := _BalanceScanner.GetBalances(address, tokens)
 	if err != nil {
 		return balances, nil
 	}
@@ -62,4 +79,66 @@ func (_BalanceScanner *BalanceScanner) TokenMapBalances(address common.Address, 
 		balances[strings.ToLower(token.String())] = res[i]
 	}
 	return balances, nil
+}
+
+func (_BalanceScanner *BalanceScanner) GetBalancesCallData(account common.Address, tokens []common.Address) ([]byte, error) {
+	return _BalanceScanner._abi.Pack("getBalances", account, tokens)
+}
+
+func (_BalanceScanner *BalanceScanner) DecodeCallResponse(data hexutil.Bytes) ([]interface{}, error) {
+	return _BalanceScanner._abi.Unpack("getBalances", data)
+}
+
+func (_BalanceScanner *BalanceScanner) BatchCallBalances(account common.Address, tokenChunks [][]common.Address) ([]*big.Int, error) {
+	var res []*big.Int
+	var calls []rpc.BatchElem
+	var callSize = len(tokenChunks)
+	var callResults = make([]hexutil.Bytes, callSize)
+	for index, chunk := range tokenChunks {
+		callData, err := _BalanceScanner.GetBalancesCallData(account, chunk)
+		if err != nil {
+			return []*big.Int{}, err
+		}
+		elem := rpc.BatchElem {
+			Method: "eth_call",
+			Args: toCallArgs(ethereum.CallMsg{To: &_BalanceScanner.ContractAddress, Data: callData}),
+			Result: &callResults[index],
+		}
+		calls = append(calls, elem)
+	}
+	err := _BalanceScanner.RpcClient.BatchCall(calls)
+	if err != nil {
+		return []*big.Int{}, err
+	}
+	for _, result := range callResults {
+		r, err := _BalanceScanner.DecodeCallResponse(result)
+		if err != nil {
+			return []*big.Int{}, err
+		}
+		res = append(res, r[0].([]*big.Int)...)
+	}
+	return res, nil
+}
+
+func toCallArgs(msg ethereum.CallMsg) []interface{} {
+	arg := []interface{}{CallArg{
+		To:       *msg.To,
+		Gas: hexutil.EncodeUint64(1000000000), // 1000000000 was used to avoid the need to config gas limit on view call
+		GasPrice: hexutil.EncodeUint64(0),
+		Value: hexutil.EncodeUint64(0),
+		Nonce: hexutil.EncodeUint64(0),
+		Data: fmt.Sprintf("0x%s", common.Bytes2Hex(msg.Data)),
+	}, toBlockNumArg(nil)}
+	return arg
+}
+
+func toBlockNumArg(number *big.Int) string {
+	if number == nil {
+		return "latest"
+	}
+	pending := big.NewInt(-1)
+	if number.Cmp(pending) == 0 {
+		return "pending"
+	}
+	return hexutil.EncodeBig(number)
 }
--- core/balance_scanner_test.go
+++ core/balance_scanner_test.go
@@ -1,20 +1,61 @@
 package core
 
 import (
-	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"fmt"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/rpc"
 	"github.com/stretchr/testify/assert"
+	"strings"
 	"testing"
 )
 
 func TestBalanceScanner(t *testing.T) {
 	client, _ := ethclient.Dial("https://bsc-dataseed1.ninicoin.io/")
-	bep20, _ := NewBalanceScanner(BinanceChain, client)
+	bep20, _ := NewBalanceScanner(BinanceChain, client, nil)
 	balances, _ := bep20.GetBalances(
-		&bind.CallOpts{},
 		common.HexToAddress("0x7d99eda556388Ad7743A1B658b9C4FC67D7A9d74"), []common.Address{
 			common.HexToAddress("0x7d99eda556388Ad7743A1B658b9C4FC67D7A9d74"),
 		})
 	assert.Equal(t, balances[0].String(), "34000000000000000000")
+}
+
+func TestBalanceScanner_TokenMapBalances(t *testing.T) {
+	client, _ := ethclient.Dial("https://bsc-dataseed1.ninicoin.io/")
+	bep20, _ := NewBalanceScanner(BinanceChain, client, nil)
+	balances, _ := bep20.TokenMapBalances(
+		common.HexToAddress("0x7d99eda556388Ad7743A1B658b9C4FC67D7A9d74"), []common.Address{
+			common.HexToAddress("0x7d99eda556388Ad7743A1B658b9C4FC67D7A9d74"),
+		})
+	assert.Equal(t, balances[strings.ToLower("0x7d99eda556388Ad7743A1B658b9C4FC67D7A9d74")].String(), "34000000000000000000")
+}
+
+func TestBalanceScanner_BatchCall(t *testing.T) {
+	client, _ := ethclient.Dial("https://bsc-dataseed.binance.org/")
+	r, _ := rpc.Dial("https://bsc-dataseed.binance.org/")
+	bep20, _ := NewBalanceScanner(BinanceChain, client, r)
+	resultCount := 0
+	ret, _ := bep20.BatchCallBalances(common.HexToAddress("0x7d99eda556388Ad7743A1B658b9C4FC67D7A9d74"), [][]common.Address{
+		{
+			common.HexToAddress("0x7d99eda556388Ad7743A1B658b9C4FC67D7A9d74"),
+			common.HexToAddress("0x7d99eda556388Ad7743A1B658b9C4FC67D7A9d74"),
+		},
+		{
+			common.HexToAddress("0x7d99eda556388Ad7743A1B658b9C4FC67D7A9d74"),
+			common.HexToAddress("0x7d99eda556388Ad7743A1B658b9C4FC67D7A9d74"),
+			common.HexToAddress("0x7d99eda556388Ad7743A1B658b9C4FC67D7A9d74"),
+		},
+		{
+			common.HexToAddress("0x7d99eda556388Ad7743A1B658b9C4FC67D7A9d74"),
+			common.HexToAddress("0x7d99eda556388Ad7743A1B658b9C4FC67D7A9d74"),
+			common.HexToAddress("0x7d99eda556388Ad7743A1B658b9C4FC67D7A9d74"),
+			common.HexToAddress("0x7d99eda556388Ad7743A1B658b9C4FC67D7A9d74"),
+			common.HexToAddress("0x7d99eda556388Ad7743A1B658b9C4FC67D7A9d74"),
+		},
+	})
+	for j := 0; j < len(ret); j++ {
+		resultCount++
+		assert.Equal(t, ret[j].String(), "34000000000000000000")
+	}
+	fmt.Println("Total result", resultCount)
 }
